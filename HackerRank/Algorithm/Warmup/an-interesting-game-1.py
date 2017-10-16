927efece3f7847a02e6e817021f0df2d22ace93e
Nandu Anil <nandua.dev@gmail.com>
2017-10-16 15:22:23 -0700

    Completed: https://www.hackerrank.com/challenges/an-interesting-game-1

--- HackerRank/Algorithm/Warmup/an-interesting-game-1.py
+++ HackerRank/Algorithm/Warmup/an-interesting-game-1.py
@@ -5,7 +5,6 @@ Created on Mon Oct 16 13:24:59 2017
 https://www.hackerrank.com/challenges/an-interesting-game-1
 
 """
-from operator import itemgetter
 
 #accept the numbe of games
 g = int(input().strip())
@@ -14,27 +13,16 @@ g = int(input().strip())
 for a in range(g):
     #number of items in this game
     n = int(input().strip())
-    gameValues = []
-    position = 0
-    for x in input().split():
-        #store each item with the value and the position
-        entry = [int(x),position]
-        gameValues.append(entry)
-        position += 1
-    
-    #sort the values in the descending order based on the value
-    sortValues = sorted(gameValues, key = itemgetter(0), reverse = True)
-    
-    #after it is sorted we loop through the sorted values.
-    #we remove one, then find the next highest value with a lower position
-    #whenever we find such a value, we increment the number of games.                                                             
+    maxSoFar = 0
     noOfGames = 0
-    currentIndexLimit = n
-    for item in sortValues:
-        if(item[1]<currentIndexLimit):
+    for x in input().split():
+        currentValue = int(x)
+        #as we read the inputs, if current item is larger than the max so far, 
+        #we can play one more game
+        if(currentValue > maxSoFar):
             noOfGames += 1
-            currentIndexLimit = item[1]
-    
+            maxSoFar = currentValue
+            
     if(noOfGames % 2 == 0 ):
         print('ANDY')
     else:
